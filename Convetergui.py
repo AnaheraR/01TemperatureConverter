6e0d35bc1c37ea75d23b27c068f077c60685991f
AnaheraR <129803740+AnaheraR@users.noreply.github.com>
2023-04-27 14:14:13 +1200

    Add files via upload

---
+++ Convetergui.py
@@ -0,0 +1,14 @@
+from tkinter import Tk
+
+
+class Converter:
+
+    def __init__(selfself):
+
+
+
+if __name__ == "__main__":
+    root = Tk()
+    root.title("Temperature Converter")
+    Converter()
+    root.mainloop()
