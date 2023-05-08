a9ff8701a8463c04955c8a4fb8c21ae5aa0723f8
AnaheraR <129803740+AnaheraR@users.noreply.github.com>
2023-05-08 12:28:49 +1200

    Add files via upload

---
+++ 04_calculations_v1.py
@@ -0,0 +1,10 @@
+def to_celsius(to_convert):
+    answer = (to_convert - 32) * 5 / 9
+    return answer
+
+
+def to_fahrenheit(to_convert):
+    answer = to_convert * 1.8 + 32
+    return answer
+
+
