8eb806dece874bbe9d2ae0c2fca420837280804a
AnaheraR <129803740+AnaheraR@users.noreply.github.com>
2023-05-09 10:58:00 +1200

    Add files via upload

--- 04_calculations_v1.py
+++ 04_calculations_v1.py
@@ -8,3 +8,15 @@ def to_fahrenheit(to_convert):
     return answer
 
 
+to_c_test = [0, 100, -459]
+to_f_test = [0, 100, 40, -273]
+
+for item in to_f_test:
+    print("{} C is {:.0f} F".format(item, to_fahrenheit(item)))
+
+print()
+
+for item in to_c_test:
+    print("{} F is {:0f} C".format(item, to_celsius(item)))
+
+print()
